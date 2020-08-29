430091ed98ae91398b315858e7a5091f66b490ad
Aran Clary Deltac <bluefeet@gmail.com>
2020-08-29 04:17:46 -0700

    Fine tuning.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function loadWorld (worldKey) {
   document.location = `${worldKey}/`;
 }
 
+function loadUrl (url) {
+  document.location = url;
+}
+
 world.startingPlayer = {
   room: 'home',
 };
@@ -12,10 +16,29 @@ const rooms = world.rooms = {};
 
 rooms.home = {
   title: 'Rocket Castle',
-  detail: 'Select a world.',
+  detail: 'Welcome to 🚀🏰!\n\nWhat would you like to do?',
   actions: [
-    ['Grungle', loadWorld, 'grungle'],
+    ['Experience.', changeRoom, 'experience'],
+    ['Create.', changeRoom, 'create'],
   ],
 };
+
+rooms.experience = {
+  title: 'Experience!',
+  detail: 'An adventure awaits...',
+  actions: [
+    ['Grungle', loadWorld, 'grungle'],
+    ['I changed my mind, go back I say!', changeRoom, 'home'],
+  ],
+};
+
+rooms.create = {
+  title: 'Create!',
+  detail: 'Want to create your own Rocket Castle experience?',
+  actions: [
+    ['I would love to, take me to your GitHub.', loadUrl, 'https://github.com/bluefeet/RocketCastle' ],
+    ['No thanks, I want to go back and make a different choice.', changeRoom, 'home'],
+  ],
+}
 
 restartGame();
